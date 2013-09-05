1230a24dd8bacb50cbf1dd201efdac1f9fee1178
apcs <apcs@mvhs-u528p31.dcsdk12.org>
2013-09-05 08:22:20 -0600

    sum/0 completed

--- warmup.java
+++ warmup.java
@@ -2,13 +2,17 @@ class warmup {
 	
 	public static void main(String[] args) {
 	
-
+		System.out.println(sum(5));
 
 	}
 
-	public static void sum(){
+	public static int sum(int n){
 
-
+		int sum = 0;
+		for (int i = n;	i>0 ; i--) {
+			sum = sum + i;
+		}
+		return sum;
 
 	}
 
@@ -24,5 +28,5 @@ class warmup {
 
 	}
 
-	
+
 }
