9e4e4b4646e21e430d2ecf64c4b10797e5ad594b
apcs <apcs@mvhs-u528p31.dcsdk12.org>
2013-09-05 08:34:26 -0600

    Finished all the math as all work without the scanners and a spefic in code input

--- warmup.java
+++ warmup.java
@@ -2,31 +2,32 @@ class warmup {
 	
 	public static void main(String[] args) {
 	
-		System.out.println(sum(5));
+		System.out.println(factorial(5));
 
 	}
 
 	public static int sum(int n){
-
 		int sum = 0;
 		for (int i = n;	i>0 ; i--) {
 			sum = sum + i;
 		}
 		return sum;
-
 	}
 
-	public static void xToTheN(){
-
-
-
+	public static int xToTheN(int x, int n){
+		int sum = 1;
+		for (int i = n; i>0; i--) {
+			sum = sum * x;
+		}
+		return sum;
 	}
 
-	public static void factorial(){
-
-
-
+	public static int factorial(int n){
+		int sum = 1;
+		for (int i = n; i > 0; i--) {
+			sum = sum * i;
+		}
+		return sum;
 	}
-
-
+	
 }
