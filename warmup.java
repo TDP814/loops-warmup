e81e9647cbdc046d2a411e6ba42b0dca8846a76b
apcs <apcs@mvhs-u528p31.dcsdk12.org>
2013-09-05 08:15:54 -0600

    main added along with the voids

---
+++ warmup.java
@@ -0,0 +1,28 @@
+class warmup {
+	
+	public static void main(String[] args) {
+	
+
+
+	}
+
+	public static void sum(){
+
+
+
+	}
+
+	public static void xToTheN(){
+
+
+
+	}
+
+	public static void factorial(){
+
+
+
+	}
+
+	
+}
